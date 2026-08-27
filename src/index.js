b99bde98aa31b64eed5626edeb2386afc78f70b9
Tanmay Mondal <tanmaymondalmondal7@gmail.com>
2026-08-27 18:06:52 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import { handlePublicBlogs } from "./routes/blogs.js";
+import { handleAdminBlogs } from "./routes/adminBlogs.js";
+import { handleAuth } from "./routes/auth.js";
+import { handleOrders } from "./routes/orders.js";
+import { handleProducts } from "./routes/products.js";
+import { handleUsers } from "./routes/users.js";
 
+// inside fetch()
+const handlers = [
+    handlePublicBlogs,
+    handleAdminBlogs,
+    handleAuth,
+    handleOrders,
+    handleProducts,
+    handleUsers,
+];
+
+for (const handler of handlers) {
+    const response = await handler(request, env, url, corsHeaders);
+    if (response) return response;
+}
+
+// remaining endpoints...
 const corsHeaders = {
 	'Access-Control-Allow-Origin': 'https://idexcy.com',
 	'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
